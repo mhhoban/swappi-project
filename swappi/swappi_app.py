210156f698adf70ac3fda69458eab6f3065adec9
Morgan Hoban <mhhoban@gmail.com>
2017-02-02 00:11:02 -0800

    laid down sqlalchemy db foundations

--- swappi/swappi_app.py
+++ swappi/swappi_app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from db_schema import Base, Categories, Items, Users
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 @app.route('/hello')
