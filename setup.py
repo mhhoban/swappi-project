4a398d45f8e6429145f3430f878cfd31de8a6e23
Morgan Hoban <mhhoban@gmail.com>
2017-02-19 19:54:28 -0800

    fixed setup.py issues.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import subprocess
-import swappi.db_setup
 
 print('Starting Setup:')
 subprocess.call('mkdir db', shell=True)
 subprocess.call('pip install -r requirements.txt', shell=True)
 
+import swappi.db_setup
 
 _db_setup = swappi.db_setup.DbSetup()
 _db_setup.db_init()
